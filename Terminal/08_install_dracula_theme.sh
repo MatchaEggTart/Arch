63e573a4200f01b62877d1f571c0f74e395c761e
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-10-22 03:35:56 +0800

    update

--- Terminal/08_install_dracula_theme.sh
+++ Terminal/08_install_dracula_theme.sh
@@ -1,6 +1,9 @@
 sudo pacman -S unzip p7zip
 cd ~/Downloads
-wget https://github.com/dracula/konsole/archive/master.zip
-7z x master.zip
-cp ~/Downloads/konsole-master/Dracula.colorscheme ~/.local/share/konsole
-
+wget -O dracula.zip https://github.com/dracula/konsole/archive/master.zip
+if [ -f "~/Downloads/dracula.zip" ];then
+    7z x dracula.zip
+    cp ~/Downloads/konsole-master/Dracula.colorscheme ~/.local/share/konsole
+else
+    echo "下载失败"
+fi
