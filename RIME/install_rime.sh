de9e261a01391c507d8b603b52c96b5efc182a71
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-11-08 14:15:49 +0800

    update

--- RIME/install_rime.sh
+++ RIME/install_rime.sh
@@ -1,8 +1,11 @@
 sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-material-color
 sudo pacman -S fcitx5-rime
 
+# Fcitx5
 # vim /etc/profile
 # export GTK_IM_MODULE=fcitx5
-# # export QT_IM_MODULE=fcitx5
-# # export XMODIFIERS=@im=fcitx
+# export QT_IM_MODULE=fcitx5
+# export XMODIFIERS=@im=fcitx
 # System Settings => Workspace Behavior => Virtual Desktop => fcitx5
+# https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
+export XMODIFIERS=@im=fcitx
