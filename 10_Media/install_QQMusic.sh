8dbe1b1ea91d412e19871a3ed81cc986dd11433b
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-11-10 12:05:56 +0800

    update

---
+++ 10_Media/install_QQMusic.sh
@@ -0,0 +1,21 @@
+sudo pacman -S fuse
+
+cd ~/Downloads
+
+wget https://dldir1.qq.com/music/clntupate/linux/AppImage/qqmusic-1.1.5.AppImage
+
+cd ~/Downloads
+
+sudo mkdir -p /opt/Tencent/QQMusic
+
+sudo mv qqmusic-1.1.5.AppImage /opt/Tencent/QQMusic
+
+cd ~/Workspace/Arch_First_Use/QQMusic/
+
+cp qqmusic.desktop ~/.local/share/applications
+
+sudo cp -r icons /opt/Tencent/QQMusic/
+
+cd /opt/Tencent/QQMusic/
+
+sudo chmod +x qqmusic-1.1.5.AppImage
