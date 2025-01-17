ad7610a3cbd9acecb091db48583cda7be7686a02
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-17 23:08:47 +0800

    update

--- Hyprland/03_wifi.sh
+++ Hyprland/03_wifi.sh
@@ -1,7 +1,20 @@
-sudo pacman -Syy iwd
+# sudo pacman -Syy iwd
+# sudo systemctl start iwd
+# sudo systemctl enable iwd
+# paru -Syy iwgtk
 
-sudo systemctl start iwd
 
-sudo systemctl enable iwd
+#!/bin/bash
 
-paru -Syy iwgtk
+echo "What's the wifi name?"
+read wifi
+# echo ${email}
+
+# echo "first: $0"
+# echo "second: $1"
+# echo "third: $2"
+
+nmcli radio wifi
+nmcli radio wifi on
+
+sudo nmcli device wifi connect "${wifi}" --ask
---
+++ Hyprland/xdg_user.sh
@@ -0,0 +1,3 @@
+sudo pacman -Syy xdg-user-dirs
+cd $HOME
+xdg-user-dirs-update
