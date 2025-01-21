6444a9dfd44aa26a221add127c5b2f2e379a6b23
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-22 01:59:31 +0800

    update

--- 07_RIME/01_install_rime.sh
+++ 07_RIME/01_install_rime.sh
@@ -2,7 +2,8 @@
 
 PATH_PROFILE="/etc/profile"
 
-sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-material-color fcitx5-rime rime-luna-pinyin rime-emoji
+# sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-material-color fcitx5-rime rime-luna-pinyin rime-emoji
+sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-rime rime-luna-pinyin rime-emoji
 
 # Fcitx5
 # vim /etc/profile
