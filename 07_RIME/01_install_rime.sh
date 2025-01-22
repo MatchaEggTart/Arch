0adc74acdae0d824561e83f310c993d63afabb24
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-22 15:16:36 +0800

    update

--- 07_RIME/01_install_rime.sh
+++ 07_RIME/01_install_rime.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH_PROFILE="/etc/profile"
+# PATH_PROFILE="/etc/profile"
 
 # sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-material-color fcitx5-rime rime-luna-pinyin rime-emoji
 sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-rime rime-luna-pinyin rime-emoji
@@ -14,5 +14,7 @@ sudo pacman -S fcitx5 fcitx5-im fcitx5-chinese-addons fcitx5-rime rime-luna-piny
 # https://fcitx-im.org/wiki/Using_Fcitx_5_on_Wayland
 # export XMODIFIERS=@im=fcitx
 
-echo -e "# Fcitx 5" | sudo tee -a $PATH_PROFILE
-echo -e "export XMODIFIERS=@im=fcitx" | sudo tee -a $PATH_PROFILE
+# echo -e "# Fcitx 5" | sudo tee -a $PATH_PROFILE
+# echo -e "export XMODIFIERS=@im=fcitx" | sudo tee -a $PATH_PROFILE
+# KDE 是 写入 export XMODIFIERS=@im=fcitx 到 /etc/profile ?
+# Hyprland 是 写入 XMODIFIERS=@im=fcitx 到 /etc/environment 
