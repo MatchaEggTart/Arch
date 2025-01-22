c60aca9c6d055667f175e1e93181e893b015942b
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-23 03:53:50 +0800

    update

---
+++ 06_Tools/04_neofetch.sh
@@ -0,0 +1,7 @@
+sudo pacman -Syy neofetch lolcat
+cd $HOME/.config/neofetch/
+git clone https://github.com/dlcgold/ascii-art-neofetch.git
+
+# neofetch | lolcat
+# alias nfl="neofetch --source ~/.config/neofetch/ascii-art-neofetch/communist | lolcat"
+# alias nfll="neofetch --source ~/.config/neofetch/ascii-art-neofetch/communist -L | lolcat"
