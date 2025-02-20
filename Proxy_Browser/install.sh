fbcb58d783035455f90f0ff086bf8f6e80f3308b
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-02-20 08:26:57 +0800

    update

---
+++ Proxy_Browser/install.sh
@@ -0,0 +1,3 @@
+paru -S google-chrome
+cp $HOME/Workspace/Arch/Proxy_Browser/proxy-chrome.desktop $HOME/.local/share/applications/proxy-chrome.desktop
+sudo cp $HOME/Workspace/Arch/Proxy_Browser/proxy-chrome.png /usr/share/icons/proxy-chrome.png
